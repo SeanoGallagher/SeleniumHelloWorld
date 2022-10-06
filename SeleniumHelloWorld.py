948e758087516a3d8b728850fb87b4c29cbd0c8d
SeanoGallagher <36253804+SeanoGallagher@users.noreply.github.com>
2022-10-06 16:22:34 -0700

    Update SeleniumHelloWorld.py
    Changed File to now search "TonDone", click on the first link, and verifies the text "The easy way to manage hard work" is on the page. If it is on the page the message "Text successfully found" will be printed in the output, otherwise "Text not found" will be printed.

--- SeleniumHelloWorld.py
+++ SeleniumHelloWorld.py
@@ -7,8 +7,14 @@ driver = webdriver.Chrome()
 driver.get('http://google.com')
 
 search = driver.find_element(By.CLASS_NAME, "gLFyf.gsfi")
-search.send_keys("Hello World")
+search.send_keys("TonDone")
 search.send_keys(Keys.RETURN)
 
-secondLink = driver.find_element(By.XPATH, "//*[@id='rso']/div[4]/div/div/div/div/a")
+secondLink = driver.find_element(By.XPATH, "//*[@id='rso']/div//a[1]")
 secondLink.click()
+
+text = driver.find_element(By.CLASS_NAME, "content-pair-text")
+if(text.text.find("The easy way to manage hard work")):
+    print("Text successfully found")
+else:
+    print("Text not found")
